658947aa2e04124907536694e8cc3003b11d0dd6
0X2045Z <zz827807@gmail.com>
2022-08-30 17:39:12 +0800

    Added

--- hello-react-fram/src/App.js
+++ hello-react-fram/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// 定义App类式组件
+import React, { Component } from 'react'
+import HelloReact from './components/hello/HelloReact'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <HelloReact/>
+      </div>
+    )
+  }
 }
 
-export default App;
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ hello-react-fram/src/components/hello/HelloReact.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './helloReact.css'
+
+export default class HelloReact extends Component {
+
+    render(){
+        return (
+            <div className='title'>
+                <h1>Hello react!!!</h1>
+            </div>
+        )
+    }
+}
